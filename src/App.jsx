5de6c4eff8009dfc7ecc88037a74ccf56e549b93
una <oksana2020la@gmail.com>
2024-04-02 01:04:40 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useEffect, useState } from 'react';
+// import { SearchBar } from './components/SearchBar/SearchBar';
+// import { getPhotos } from './api';
+// import { ImageGallery } from './components/ImageGallery/ImageGallery';
+// import { LoadMoreBtn } from './components/LoadMoreBtn/LoadMoreBtn';
+// import { Toaster } from 'react-hot-toast';
+// import { Loader } from './components/Loader/Loader';
+// import { ErrorMessage } from './components/ErrorMessage/ErrorMessage';
+// import css from './components/ErrorMessage/ErrorMessage.module.css';
+// import style from './components/Loader/Loader.module.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [query, setQuery] = useState('');
+//   const [page, setPage] = useState(1);
+//   const [images, setImages] = useState([]);
+//   const [emptyResults, setEmptyResults] = useState(false);
+//   const [visibleBtn, setVisibleBtn] = useState(false);
+//   const [error, setError] = useState(null);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [emptyInput, setEmptyInput] = useState(false);
+//   }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+// export default App;
 
-export default App
+
+
+    
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
